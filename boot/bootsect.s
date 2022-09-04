ec1913fecd29663a15c6fc34cc7f003d4922aee8
horbyn <hor4bynZ@gmail.com>
2022-09-04 18:01:13 +0800

    bootsect v2 done

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,75 +1,63 @@
 #       Copyright (C)    horbyn, 2022
 #             (hoRbyn4zZ@outlook.com)
 
-	.text
-	.globl start
-	.code16
+    .text
+    .globl _start
+    .code16
 
-	.set SEC_NR,         3   # 896 # the amount of sector to be loaded (≈448KB)
-	.set SEC_144M,       18  # the specification for sector of 1.44M floppy
-	.set TRK_144M,       80  # the specification for track of 1.44M floppy
-	.set HEAD_144M,      2   # the specification for head of 1.44M floppy
+	.set SEC_NR,       3        # 896 # the amount of sector to be loaded (≈448KB)
+	.set SEC_144M,     18       # the specification for sector of 1.44M floppy
+	.set TRK_144M,     80       # the specification for track of 1.44M floppy
+	.set HEAD_144M,    2        # the specification for head of 1.44M floppy
 
 	# system data buffer
-    .set MM_KSTACK_BUF,  0x9800
-    .set MM_NEW_MBR_SEG, 0x8000
-    .set MM_DIED_OFF,    0xfffe
-    .set MM_GDT_OFF,     0x104
-    .set MM_CURSOR_OFF,  0x100
-	.set MEM_ST_OFF,     4   # ARDS struct base address
-	.set MEM_NR_OFF,     0   # ARDS amount
+    .set SEG_KERN,     0x1000
+    .set SEG_MBR,      0xf00
+    .set OFF_GDT,      0x198    # gdt base
+    .set OFF_ARDS,     8        # ARDS struct base address
+    .set OFF_ARDS_CR,  4        # ARDS amount
+    .set OFF_CURSOR,   0        # cursor location
+    .set SEG_STACK,    0
+    .set STACK_SP,     0xf000
+    .set OFF_DIED,     0x77fc   # died instruction
 
-	# temp data
-	.set TEMP_SYS_SEG,   0x1000
+_start:
+	movw $SEG_MBR,     %ax
+	movw %ax,          %ds
+	movw $SEG_STACK,   %ax
+	movw %ax,          %ss
+	movw $STACK_SP,    %sp      # stack is set to 0:0xf000
 
-start:
-	movw %cs,             %ax
-	movw %ax,             %ds
-	movw $MM_NEW_MBR_SEG, %ax
-	movw %ax,             %ss
-	movw $0x7c00,         %sp # stack is set to 0x87c00
-	
-	## function 1.
-	## ds:si -> es:di (0:0x7c00 -> 0x8000:0x7c00)
-	cld
-	movw $MM_NEW_MBR_SEG, %ax
-	movw %ax,             %es
-	movl $0x7c00,         %esi
-	movl $0x7c00,         %edi
-	movl $0x100,          %ecx
-	rep  movsw
-
-	ljmp $MM_NEW_MBR_SEG, $go
-go:
-
-    ## function 2.
-	## Memory detection
-	movw $MM_NEW_MBR_SEG, %ax
-	movw %ax,             %ds
-	movl $0,              %ebx
-	movl $0x14,           %ecx
-	movw $MEM_ST_OFF,     %di # ARDS struct base address
-	movl $0,              MEM_NR_OFF # ARDS amount
+	# ####    function 1.   ####
+	# #### Memory detection ####
+    movl $0,           OFF_ARDS_CR  # init ards counts
+    movw $SEG_MBR,     %ax
+    movw %ax,          %es
+    movw $OFF_ARDS,    %di      # es:di the dest
+	movl $0,           %ebx
+	movl $0x14,        %ecx
 detect_mem:
-	movl $0xe820,         %eax
-	movl $0x534d4150,     %edx
-	int  $0x15
-	## check
-	jnb cf0 # judge cf1? (1 i.e. error)
-	jmp  . # error happened
-cf0:
-	incl MEM_NR_OFF
-	addw $0x14,           %di
-	cmp  $0,              %ebx
-	jne  detect_mem # judge zf1? (1 i.e. detect completed)
+    movl $0xe820,      %eax
+    movl $0x534d4150,  %edx
+    int  $0x15
 
-	## function 3.
-	## kernel loading from disk to memory(0x10000)
-	movw $TEMP_SYS_SEG,   %ax
-	movw %ax,             %es
-	xorw %bx,             %bx # load to 0x1000:0
+    jnb  cf0 # judge cf1? (1 i.e. error)
+    jmp  . # error happened
+cf0:
+    incl OFF_ARDS_CR
+    addw $0x14,        %di
+    cmp  $0,           %ebx
+    jne  detect_mem # judge zf1? (1 i.e. detect completed)
+
+    # ####              function 2.                 ####
+    # #### kernel loading from disk to mm.(0x10000) ####
+	movw %cs,          %ax
+	movw %ax,          %ds
+    movw $SEG_KERN,    %ax
+    movw %ax,          %es
+    xorw %bx,          %bx # load to 0x1000:0
 load_sect:
-	movw lba_base,        %ax
+    movw lba_base,        %ax
 	movb $SEC_144M,       %dl
 	divb %dl
 	addb $1,              %ah
@@ -108,61 +96,56 @@ after_reset:
 	jmp  load_sect
 
 load_sect_ok:
-	## function 4.
-	## Move gdt to 0x80104
-	cld
-	movw $MM_NEW_MBR_SEG, %ax
-	movw %ax,             %es
+    # ####     function 3.     ####
+    # #### System parameters initialization ####
+    cld
+    movw %cs,             %ax
+    movw %ax,             %ds
+    movw $SEG_MBR,        %ax
+    movw %ax,             %es
 	movl $gdt,            %esi
-	movl $MM_GDT_OFF,     %edi
+	movl $OFF_GDT,        %edi
 	movl $0xa,            %ecx
-	rep  movsl
+	rep  movsl # Move gdt to 0xf198
+    movl died,           %eax
+    movl %eax,           OFF_DIED # died instruction
+	movw $SEG_MBR,       %ax
+	movw %ax,            %ds
+    movl $0,             OFF_CURSOR # global cursor location
 
-	## function 5.
-	## Move the whole kernel from 0x10000 to 0
-	movw $TEMP_SYS_SEG,   %ax
-	movw %ax,             %ds
-	movw $0,              %ax
-	movw %ax,             %es
-	xorl %esi,            %esi
-	xorl %edi,            %edi
-	movl $SEC_NR<<7,      %ecx
-	rep  movsl
+    # #### function 4. ####
+    # #### Forge a stack environment to call lret
+    pushl $0x08	# cs seletor in future
+    pushl $OFF_DIED
 
-	## function 6.
-	## System parameters initialization
-	movw $MM_NEW_MBR_SEG, %ax # restore ds
-	movw %ax,             %ds
-	movl $0,              MM_CURSOR_OFF # global cursor
-	movl died,            %eax # died instruction
-	movl %eax,            MM_DIED_OFF
-
-	## function 7.
-	## Forge a stack environment to call lret
-	movl $MM_KSTACK_BUF,     %eax
-	movl %eax,               %ss
-	movl $0,                 %esp
-	movl $MM_NEW_MBR_SEG<<4, %eax
-	addl $MM_DIED_OFF,       %eax
-	pushl $0x08	# cs seletor in future
-	pushl %eax
-
-	## function 8.
-	## Enter P.M.
-	cli
-	inb  $0x92,       %al	# open A20
-	orb  $2,          %al
-	outb %al,         $0x92
+    # #### function 5. ####
+    # #### enter p.m. ####
+    cli
+	movw %cs,             %ax
+    movw %ax,             %ds
+	inb  $0x92,              %al	# open A20
+	orb  $2,                 %al
+	outb %al,                $0x92
 
 	lgdt gdt_48 
 
-	movl %cr0,        %eax	# enable p.m.
-	orl  $1,          %eax
-	movl %eax,        %cr0
-	
-	.code32
-	ljmp $0x08,       $0
-	# ######### end boot ###########
+	movl %cr0,               %eax	# enable p.m.
+	orl  $1,                 %eax
+	movl %eax,               %cr0
+
+    ljmp $0x08, $pm_go # refresh pipeline
+
+pm_go:
+    .code32
+	movw $0x10,              %ax
+	movw %ax,                %ds
+	movw %ax,                %es
+	movw %ax,                %fs
+	movw %ax,                %gs
+	movw %ax,                %ss
+
+    ljmp $0x08, $SEG_KERN<<4
+    # ######### end boot ###########
 
 died:
 	jmp  .
@@ -177,7 +160,7 @@ gdt:
 	.quad 0x0000000000000000
 gdt_48:
 	.word .-gdt
-	.long 0x90000 # gdt base
+	.long 0xf198 # gdt base(SEG_MBR<<4 + OFF_GDT)
 
 .org	0x1fe, 0x90
 .word	0xaa55
